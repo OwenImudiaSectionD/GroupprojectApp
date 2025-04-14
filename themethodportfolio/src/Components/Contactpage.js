d152d1c717f5390988e797a24af7b9f6f969af8a
IsaacWilliamsSectionD <n01680206@humber.ca>
2025-04-14 11:01:17 -0400

    The Foundation of the omniverse

--- themethodportfolio/src/Components/Contactpage.js
+++ themethodportfolio/src/Components/Contactpage.js
@@ -1,7 +1,16 @@
-export default function Contactpage(){
-    return(
-        <>
-        
-        </>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Contactpage() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Here's how you can get in touch with us.</p>
+      {/* Add contact form or details here */}
+      <br />
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
 }
+
+export default Contactpage;
